1b2d8ec0b66fa68a338bf777866c8ecd63a4b3c5
Dmitry Sushchevsky <d.suschevsky.personal@gmail.com>
2023-02-04 22:05:15 +0300

    Infrastructure - Added AppDbContext, EfRepository, Configurations for User entity

---
+++ Backend/Interview.Infrastructure/Database/Configurations/UserTypeConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using Interview.Domain.Users;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Interview.Infrastructure.Database.Configurations;
+
+public sealed class UserTypeConfiguration : EntityTypeConfigurationBase<User>
+{
+    protected override void ConfigureCore(EntityTypeBuilder<User> builder)
+    {
+        builder.Property(e => e.Nickname).IsRequired().HasMaxLength(128);
+    }
+}
