bc9fb6281a7e2b20fbcdb6c56ce4bf4e2402c389
Dmitry Sushchevsky <d.suschevsky.personal@gmail.com>
2023-02-10 09:21:42 +0300

    Infrastructure, ReloadableCacheTwitchTokenProvider - Removed MemoryCache

--- Backend/Interview.Infrastructure/Chat/TokenProviders/ReloadableCacheTwitchTokenProvider.cs
+++ Backend/Interview.Infrastructure/Chat/TokenProviders/ReloadableCacheTwitchTokenProvider.cs
@@ -1,19 +1,14 @@
-﻿using Microsoft.Extensions.Caching.Memory;
-
-namespace Interview.Infrastructure.Chat.TokenProviders;
+﻿namespace Interview.Infrastructure.Chat.TokenProviders;
 
 public sealed class ReloadableCacheTwitchTokenProvider : ITwitchTokenProvider
 {
-    private const string CacheKey = "ReloadableCacheTwitchTokenProvider";
-    
+    private TwitchToken? _cacheToken;
     private readonly ITwitchTokenProvider _original;
-    private readonly IMemoryCache _memoryCache;
     private readonly SemaphoreSlim _semaphore;
 
-    public ReloadableCacheTwitchTokenProvider(ITwitchTokenProvider original, IMemoryCache memoryCache)
+    public ReloadableCacheTwitchTokenProvider(ITwitchTokenProvider original)
     {
         _original = original;
-        _memoryCache = memoryCache;
         _semaphore = new SemaphoreSlim(1, 1);
     }
 
@@ -34,26 +29,12 @@ public sealed class ReloadableCacheTwitchTokenProvider : ITwitchTokenProvider
 
     private async ValueTask<TwitchToken> GetTokenCoreAsync(CancellationToken cancellationToken)
     {
-        if (!_memoryCache.TryGetValue(CacheKey, out TwitchToken twitchToken))
-        {
-            // Cache miss
-            twitchToken = await AddTokenToCacheAsync(cancellationToken);
-        }
-
-        if (twitchToken.Expired)
-        {
-            _memoryCache.Remove(CacheKey);
-            twitchToken = await AddTokenToCacheAsync(cancellationToken);
-        }
-
+        _cacheToken ??= await _original.GetTokenAsync(cancellationToken);
+        var twitchToken = _cacheToken.Value;
+        if (!twitchToken.Expired) 
+            return twitchToken;
+        // Refresh
+        _cacheToken = twitchToken = await _original.GetTokenAsync(cancellationToken);
         return twitchToken;
-    }
-
-    private async ValueTask<TwitchToken> AddTokenToCacheAsync(CancellationToken cancellationToken)
-    {
-        var entry = _memoryCache.CreateEntry(CacheKey);
-        var token = await _original.GetTokenAsync(cancellationToken);
-        entry.Value = token;
-        return token;
     }
 }
