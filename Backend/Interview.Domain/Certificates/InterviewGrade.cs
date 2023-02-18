f145d95521c5d42171f62921cf4f6d6758a5100a
Dmitry Sushchevsky <d.suschevsky.personal@gmail.com>
2023-02-18 20:12:28 +0300

    Backend, Linter - Fixed InterviewGrade and ReactionType

--- Backend/Interview.Domain/Certificates/InterviewGrade.cs
+++ Backend/Interview.Domain/Certificates/InterviewGrade.cs
@@ -5,9 +5,13 @@ namespace Interview.Domain.Certificates;
 public sealed class InterviewGrade : SmartEnum<InterviewGrade>
 {
     public static readonly InterviewGrade Awful = new ("Ужасно", 0);
+
     public static readonly InterviewGrade Bad = new ("Плохо", 1);
+
     public static readonly InterviewGrade NotBad = new ("Не плохо", 2);
+
     public static readonly InterviewGrade Good = new ("Хорошо", 3);
+
     public static readonly InterviewGrade Perfect = new ("Превосходно", 4);
 
     private InterviewGrade(string name, int value)
