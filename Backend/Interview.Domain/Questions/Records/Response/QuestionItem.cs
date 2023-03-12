c471f8ec5bc7e6beeafe1a0b84d396fe51bb69d6
Павел Талайко <devpavdeveloper@yandex.ru>
2023-03-12 22:45:41 +0300

    feature/#49 - Add method FindById for question

---
+++ Backend/Interview.Domain/Questions/Records/Response/QuestionItem.cs
@@ -0,0 +1,9 @@
+namespace Interview.Domain.Questions.Records.Response
+{
+    public class QuestionItem
+    {
+        public Guid Id { get; init; }
+
+        public string Value { get; init; }
+    }
+}
