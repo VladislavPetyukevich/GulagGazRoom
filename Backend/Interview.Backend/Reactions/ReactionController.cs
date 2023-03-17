4850c8a0d7f2b0ebcbc8494a369960f13f4249c5
Павел Талайко <devpavdeveloper@yandex.ru>
2023-03-17 21:27:21 +0300

    feature/#67 Добавлено получение страниц реакций  (#76)
    * feature/#67
    - Add get page for reactions
    
    * feature/#67
    - Delete return async -> await
    - Add cancellation token for method

---
+++ Backend/Interview.Backend/Reactions/ReactionController.cs
@@ -0,0 +1,27 @@
+using Interview.Backend.Auth;
+using Interview.Backend.Shared;
+using Interview.Domain.Reactions.Records;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using X.PagedList;
+
+namespace Interview.Backend.Reactions
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ReactionController : ControllerBase
+    {
+        private readonly ReactionService _reactionService;
+
+        public ReactionController(ReactionService reactionService)
+        {
+            _reactionService = reactionService;
+        }
+
+        [Authorize(policy: GulagSecurePolicy.User)]
+        [HttpGet(nameof(GetPage))]
+        [ProducesResponseType(typeof(IPagedList<ReactionDetail>), 200)]
+        public Task<IPagedList<ReactionDetail>> GetPage([FromQuery] PageRequest request) =>
+            _reactionService.GetPageAsync(request.PageNumber, request.PageSize, HttpContext.RequestAborted);
+    }
+}
