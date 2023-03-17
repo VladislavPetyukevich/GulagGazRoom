551644bc31005bb0f93a36233dafbb6bd4d335c4
Павел Талайко <devpavdeveloper@yandex.ru>
2023-03-17 20:10:56 +0300

    feature/#62 - Fix identity user in claims

--- Backend/Interview.Backend/Auth/ClaimsPrincipalExt.cs
+++ Backend/Interview.Backend/Auth/ClaimsPrincipalExt.cs
@@ -4,13 +4,15 @@ namespace Interview.Backend.Auth;
 
 public static class ClaimsPrincipalExt
 {
+    private const string UserId = "USER_ID";
+    
     public static void EnrichRolesWithId(this ClaimsPrincipal self, User user)
     {
         var newRoles = user.Roles.Select(e => new Claim(ClaimTypes.Role, e.Name.Name));
 
         var claimIdentity = new ClaimsIdentity(newRoles);
 
-        claimIdentity.AddClaim(new Claim(ClaimTypes.UserData, user.Id.ToString()));
+        claimIdentity.AddClaim(new Claim(UserId, user.Id.ToString()));
 
         self.AddIdentity(claimIdentity);
     }
@@ -24,7 +26,7 @@ public static class ClaimsPrincipalExt
             return null;
         }
 
-        var id = self.Claims.FirstOrDefault(e => e.Type == ClaimTypes.UserData);
+        var id = self.Claims.FirstOrDefault(e => e.Type == UserId);
         var user = new User(nickname.Value, twitchId.Value);
         if (id != null && Guid.TryParse(id.Value, out var typedId))
         {
