29f411588df4c8602eae3aca67e9245a8edbc329
Павел Талайко <devpavdeveloper@yandex.ru>
2023-03-18 09:28:37 +0300

    feature/#83 - Create participant endpoint

--- Backend/Interview.Backend/RoomParticipants/RoomParticipantController.cs
+++ Backend/Interview.Backend/RoomParticipants/RoomParticipantController.cs
@@ -1,5 +1,6 @@
 using Interview.Backend.Auth;
 using Interview.Domain.RoomParticipants.Records;
+using Interview.Domain.RoomParticipants.Records.Request;
 using Interview.Domain.RoomParticipants.Service;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -24,6 +25,24 @@ namespace Interview.Backend.RoomParticipants
         {
             var participantResult =
                 await _roomParticipantService.ChangeParticipantStatusAsync(request, HttpContext.RequestAborted);
+
+            if (participantResult.IsFailure)
+            {
+                return NotFound(participantResult.Error);
+            }
+
+            return Ok(participantResult.Value);
+        }
+
+        [Authorize(policy: GulagSecurePolicy.Manager)]
+        [HttpPatch(nameof(ChangeParticipantStatus))]
+        [ProducesResponseType(typeof(RoomParticipantDetail), 200)]
+        [ProducesResponseType(typeof(string), 404)]
+        public async Task<ActionResult<RoomParticipantDetail?>> CreateParticipant(
+            [FromBody] RoomParticipantCreateRequest request)
+        {
+            var participantResult =
+                await _roomParticipantService.CreateParticipantAsync(request, HttpContext.RequestAborted);
 
             if (participantResult.IsFailure)
             {
