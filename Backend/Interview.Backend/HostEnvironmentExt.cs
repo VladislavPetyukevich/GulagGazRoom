866705d3f67717f471e773a2b6b484fddc072053
Павел Талайко <devpavdeveloper@yandex.ru>
2023-03-18 16:19:03 +0300

    Feature/#53 (#93)
    * feature/docker
    - Add backend Dockerfile
    - Add postgres Dockerfile
    - Add nginx Dockerfile
    - Add docker compose
    
    * feature/#63
    - Delete init database
    
    * feature/#53
    - Delete init database
    
    * feature/#53
    - Change conf postgresql
    
    * feature/#53
    - Add init.sql
    
    * Backend, PreProduction env - Added environment
    
    * Backend, Swagger - Enable for all environment
    
    * Backend, appsettings.PreProduction.json - Added file
    
    * feature/#53
    - Delete init database
    
    ---------
    
    Co-authored-by: Dmitry Sushchevsky <d.suschevsky.personal@gmail.com>

---
+++ Backend/Interview.Backend/HostEnvironmentExt.cs
@@ -0,0 +1,11 @@
+﻿namespace Interview.Backend
+{
+    public static class HostEnvironmentExt
+    {
+        public static bool IsPreProduction(this IHostEnvironment hostEnvironment)
+        {
+            ArgumentNullException.ThrowIfNull(hostEnvironment);
+            return hostEnvironment.IsEnvironment("PreProduction");
+        }
+    }
+}
