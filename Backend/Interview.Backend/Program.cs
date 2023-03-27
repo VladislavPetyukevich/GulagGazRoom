6efb83d029d3bd803fed7b085429807b923584e3
Павел Талайко <devpavdeveloper@yandex.ru>
2023-03-27 22:58:25 +0300

    Add UseForwardedHeaders

--- Backend/Interview.Backend/Program.cs
+++ Backend/Interview.Backend/Program.cs
@@ -1,6 +1,7 @@
 using Interview.Backend;
 
 using Interview.Infrastructure.Database;
+using Microsoft.AspNetCore.HttpOverrides;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddEnvironmentVariables("INTERVIEW_BACKEND_");
@@ -10,6 +11,11 @@ var serviceConfigurator = new ServiceConfigurator(builder.Environment, builder.C
 serviceConfigurator.AddServices(builder.Services);
 
 var app = builder.Build();
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedProto,
+});
 
 using (var serviceScope = app.Services.CreateScope())
 {
