3796831e00a8d732ca0a1c9a10ea7f90a495f3f3
Павел Талайко <devpavdeveloper@yandex.ru>
2023-04-02 19:20:39 +0300

    feature/#140 - Init PreProduction database

--- Backend/Interview.Migrations.Postgres/AppDbContextFactory.cs
+++ Backend/Interview.Migrations.Postgres/AppDbContextFactory.cs
@@ -9,6 +9,7 @@ namespace Interview.Migrations.Postgres
     {
         public AppDbContext CreateDbContext(string[] args)
         {
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
             var builder = new DbContextOptionsBuilder<AppDbContext>()
                 .UseNpgsql("Host=localhost;Username=devpav;Password=devpav;Database=postgres", b => b.MigrationsAssembly(typeof(AppDbContextFactory).Assembly.FullName));
             return new AppDbContext(builder.Options, Array.Empty<IChangeEntityProcessor>());
