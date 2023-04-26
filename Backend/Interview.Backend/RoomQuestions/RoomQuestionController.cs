580eb81ef243a873e1e9903ba1412284bde1471c
Павел Талайко <devpavdeveloper@yandex.ru>
2023-04-26 08:01:23 +0300

    feature/#159 (#160)
    - Refactoring controllers

--- Backend/Interview.Backend/RoomQuestions/RoomQuestionController.cs
+++ Backend/Interview.Backend/RoomQuestions/RoomQuestionController.cs
@@ -6,49 +6,70 @@ using Interview.Domain.RoomQuestions.Records.Response;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Interview.Backend.RoomQuestions
+namespace Interview.Backend.RoomQuestions;
+
+[ApiController]
+[Route("room-questions")]
+public class RoomQuestionController : ControllerBase
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class RoomQuestionController : ControllerBase
+    private readonly RoomQuestionService _roomQuestionService;
+
+    public RoomQuestionController(RoomQuestionService roomQuestionService)
     {
-        private readonly RoomQuestionService _roomQuestionService;
+        _roomQuestionService = roomQuestionService;
+    }
 
-        public RoomQuestionController(RoomQuestionService roomQuestionService)
-        {
-            _roomQuestionService = roomQuestionService;
-        }
+    /// <summary>
+    /// Changing the current question.
+    /// </summary>
+    /// <param name="request">User Request.</param>
+    /// <returns>Data of the current question, room, status.</returns>
+    [Authorize(policy: GulagSecurePolicy.Manager)]
+    [HttpPut("active-question")]
+    [Produces("application/json")]
+    [ProducesResponseType(typeof(RoomQuestionDetail), StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(MessageResponse), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(MessageResponse), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(MessageResponse), StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(typeof(MessageResponse), StatusCodes.Status500InternalServerError)]
+    public Task<ActionResult<RoomQuestionDetail>> ChangeActiveQuestion(RoomQuestionChangeActiveRequest request)
+    {
+        return _roomQuestionService.ChangeActiveQuestionAsync(request, HttpContext.RequestAborted).ToResponseAsync();
+    }
 
-        [Authorize(policy: GulagSecurePolicy.Manager)]
-        [HttpPost(nameof(ChangeActiveQuestion))]
-        [ProducesResponseType(typeof(RoomQuestionDetail), StatusCodes.Status201Created)]
-        [ProducesResponseType(typeof(MessageResponse), StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(typeof(MessageResponse), StatusCodes.Status404NotFound)]
-        public Task<ActionResult<RoomQuestionDetail>> ChangeActiveQuestion(
-            RoomQuestionChangeActiveRequest request)
-        {
-            return _roomQuestionService.ChangeActiveQuestionAsync(request, HttpContext.RequestAborted).ToResponseAsync();
-        }
+    /// <summary>
+    /// Creating a question in a room.
+    /// </summary>
+    /// <param name="request">User Request.</param>
+    /// <returns>Data of the current question, room, status.</returns>
+    [Authorize(policy: GulagSecurePolicy.Manager)]
+    [HttpPost]
+    [Produces("application/json")]
+    [ProducesResponseType(typeof(RoomQuestionDetail), StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(MessageResponse), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(MessageResponse), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(MessageResponse), StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(typeof(MessageResponse), StatusCodes.Status500InternalServerError)]
+    public Task<ActionResult<RoomQuestionDetail>> Create(RoomQuestionCreateRequest request)
+    {
+        return _roomQuestionService.CreateAsync(request, HttpContext.RequestAborted).ToResponseAsync();
+    }
 
-        [Authorize(policy: GulagSecurePolicy.Manager)]
-        [HttpPost(nameof(Create))]
-        [ProducesResponseType(typeof(RoomQuestionDetail), StatusCodes.Status201Created)]
-        [ProducesResponseType(typeof(MessageResponse), StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(typeof(MessageResponse), StatusCodes.Status404NotFound)]
-        public Task<ActionResult<RoomQuestionDetail>> Create(
-            RoomQuestionCreateRequest request)
-        {
-            return _roomQuestionService.CreateAsync(request, HttpContext.RequestAborted).ToResponseAsync();
-        }
-
-        [Authorize]
-        [HttpGet(nameof(GetRoomQuestions))]
-        [ProducesResponseType(typeof(List<Guid>), StatusCodes.Status200OK)]
-        [ProducesResponseType(typeof(List<Guid>), StatusCodes.Status204NoContent)]
-        [ProducesResponseType(typeof(MessageResponse), StatusCodes.Status404NotFound)]
-        public Task<ActionResult<List<Guid>>> GetRoomQuestions([FromQuery] RoomQuestionsRequest request)
-        {
-            return _roomQuestionService.GetRoomQuestionsAsync(request).ToResponseAsync();
-        }
+    /// <summary>
+    /// Getting a page with room questions.
+    /// </summary>
+    /// <param name="request">User Request.</param>
+    /// <returns>The page with the questions of the room.</returns>
+    [Authorize]
+    [HttpGet]
+    [Produces("application/json")]
+    [ProducesResponseType(typeof(List<Guid>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(MessageResponse), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(MessageResponse), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(MessageResponse), StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(typeof(MessageResponse), StatusCodes.Status500InternalServerError)]
+    public Task<ActionResult<List<Guid>>> FindRoomQuestions([FromQuery] RoomQuestionsRequest request)
+    {
+        return _roomQuestionService.FindRoomQuestionsAsync(request).ToResponseAsync();
     }
 }
