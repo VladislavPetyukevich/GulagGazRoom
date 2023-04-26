00515b1f9a3a25817738498815792f0924a16441
Dmitry Sushchevsky <d.suschevsky.personal@gmail.com>
2023-04-26 19:21:53 +0300

    Backend, User - Resolved #161

--- Backend/Interview.Backend/Program.cs
+++ Backend/Interview.Backend/Program.cs
@@ -24,6 +24,20 @@ using (var serviceScope = app.Services.CreateScope())
 {
     var appDbContext = serviceScope.ServiceProvider.GetRequiredService<AppDbContext>();
     appDbContext.Database.Migrate();
+    var testUserId = Guid.Parse("b5a05f34-e44d-11ed-b49f-e8e34e3377ec");
+    if (app.Environment.IsDevelopment() && !appDbContext.Users.Any(e => e.Id == testUserId))
+    {
+        appDbContext.Users.Add(new User("TEST_BACKEND_DEV_USER", "d1731c50-e44d-11ed-905c-d08c09609150")
+        {
+            Id = testUserId,
+            Avatar = null,
+            Roles =
+            {
+                appDbContext.Roles.Find(RoleName.User.Id) !,
+            },
+        });
+        appDbContext.SaveChanges();
+    }
 }
 
 var middlewareConfigurator = new MiddlewareConfigurator(app);
