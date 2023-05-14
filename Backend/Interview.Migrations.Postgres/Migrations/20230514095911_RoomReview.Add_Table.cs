b1159773f459fa9c6c82688a7520268100f5d2a7
Павел Талайко <devpavdeveloper@yandex.ru>
2023-05-14 13:21:37 +0300

    feature/#112 - Dotnet format

--- Backend/Interview.Migrations.Postgres/Migrations/20230514095911_RoomReview.Add_Table.cs
+++ Backend/Interview.Migrations.Postgres/Migrations/20230514095911_RoomReview.Add_Table.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
@@ -39,7 +39,7 @@ namespace Interview.Migrations.Postgres.Migrations
                         principalColumn: "Id",
                         onDelete: ReferentialAction.Cascade);
                 });
-            
+
             migrationBuilder.CreateIndex(
                 name: "IX_RoomReview_RoomId",
                 table: "RoomReview",
