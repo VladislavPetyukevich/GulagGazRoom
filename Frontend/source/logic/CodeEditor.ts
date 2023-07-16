ff013867e1cdc44514ad81ff7c2944c8689741e4
VladislavPetyukevich <pet.vladislav@gmail.com>
2023-07-16 17:38:58 +0300

    Frontend, Admin - Language selector for code editor

--- Frontend/source/logic/CodeEditor.ts
+++ Frontend/source/logic/CodeEditor.ts
@@ -1,7 +1,8 @@
 import * as monaco from 'monaco-editor';
 
 export class CodeEditor {
-  container: HTMLElement
+  container: HTMLElement;
+  languageSelect?: HTMLSelectElement;
   editor: monaco.editor.IStandaloneCodeEditor;
   onChange?: (value: string) => void;
 
@@ -18,10 +19,10 @@ export class CodeEditor {
     this.editor = monaco.editor.create(container, {
       fontSize: 22,
       theme: 'vs-dark',
-      language: 'javascript',
       automaticLayout: true,
       minimap: { enabled: false },
     });
+    this.createLanguagesSelect(monaco.languages.getLanguages());
     this.editor.onDidChangeModelContent(() => {
       if (this.ignoreChange) {
         this.ignoreChange = false;
@@ -53,4 +54,38 @@ export class CodeEditor {
   setReadonly(value: boolean) {
     this.editor.updateOptions({ readOnly: value });
   }
+
+  createLanguagesSelect(languages: monaco.languages.ILanguageExtensionPoint[]) {
+    this.languageSelect = document.createElement('select');
+    this.languageSelect.onchange = this.handleLanguageChange;
+    for (let language of languages) {
+      const option = document.createElement('option');
+      option.textContent = this.getLanguageDisplayName(language);
+      option.value = language.id;
+      this.languageSelect.appendChild(option);
+    }
+    this.container.appendChild(this.languageSelect);
+  }
+
+  getLanguageDisplayName(language: monaco.languages.ILanguageExtensionPoint) {
+    const alias = language.aliases && language.aliases[0];
+    if (alias) {
+      return alias;
+    }
+    return language.id;
+  }
+
+  handleLanguageChange = () => {
+    if (!this.languageSelect) {
+      console.error('Change language error: language select not found');
+      return;
+    }
+    const languageId = this.languageSelect.value;
+    const model = this.editor.getModel();
+    if (!model) {
+      console.error('Change language error: editor model not found');
+      return;
+    }
+    monaco.editor.setModelLanguage(model, languageId);
+  };
 }
