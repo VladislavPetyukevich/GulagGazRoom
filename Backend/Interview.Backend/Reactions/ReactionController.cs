914fc32461e8fdf522e733412474b9ebafe23741
Павел Талайко <devpavdeveloper@yandex.ru>
2023-07-31 22:57:13 +0300

    feature/#196 - Change controllers ReactionController.cs - Change controllers RoomParticipantController.cs

--- Backend/Interview.Backend/Reactions/ReactionController.cs
+++ Backend/Interview.Backend/Reactions/ReactionController.cs
@@ -1,27 +1,36 @@
 using Interview.Backend.Auth;
+using Interview.Backend.Responses;
 using Interview.Domain;
+using Interview.Domain.Questions.Records.Response;
 using Interview.Domain.Reactions.Records;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using X.PagedList;
 
-namespace Interview.Backend.Reactions
+namespace Interview.Backend.Reactions;
+
+[ApiController]
+[Route("reactions")]
+public class ReactionController : ControllerBase
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class ReactionController : ControllerBase
+    private readonly ReactionService _reactionService;
+
+    public ReactionController(ReactionService reactionService)
     {
-        private readonly ReactionService _reactionService;
-
-        public ReactionController(ReactionService reactionService)
-        {
-            _reactionService = reactionService;
-        }
-
-        [Authorize(policy: GulagSecurePolicy.User)]
-        [HttpGet(nameof(GetPage))]
-        [ProducesResponseType(typeof(IPagedList<ReactionDetail>), StatusCodes.Status200OK)]
-        public Task<IPagedList<ReactionDetail>> GetPage([FromQuery] PageRequest request) =>
-            _reactionService.GetPageAsync(request.PageNumber, request.PageSize, HttpContext.RequestAborted);
+        _reactionService = reactionService;
     }
+
+    /// <summary>
+    /// Getting a list of reactions
+    /// </summary>
+    /// <param name="request">Page Parameters</param>
+    /// <returns>List of reactions</returns>
+    [HttpGet]
+    [Authorize(policy: GulagSecurePolicy.User)]
+    [ProducesResponseType(typeof(IPagedList<ReactionDetail>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(MessageResponse), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(MessageResponse), StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(typeof(MessageResponse), StatusCodes.Status500InternalServerError)]
+    public Task<IPagedList<ReactionDetail>> GetPage([FromQuery] PageRequest request) =>
+        _reactionService.GetPageAsync(request.PageNumber, request.PageSize, HttpContext.RequestAborted);
 }
--- Backend/Interview.Backend/RoomParticipants/RoomParticipantController.cs
+++ Backend/Interview.Backend/RoomParticipants/RoomParticipantController.cs
@@ -6,48 +6,70 @@ using Interview.Domain.RoomParticipants.Service;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Interview.Backend.RoomParticipants
+namespace Interview.Backend.RoomParticipants;
+
+[ApiController]
+[Route("room-participants")]
+public class RoomParticipantController : ControllerBase
 {
-    public class RoomParticipantController : ControllerBase
+    private readonly RoomParticipantService _roomParticipantService;
+
+    public RoomParticipantController(RoomParticipantService roomParticipantService)
     {
-        private readonly RoomParticipantService _roomParticipantService;
+        _roomParticipantService = roomParticipantService;
+    }
 
-        public RoomParticipantController(RoomParticipantService roomParticipantService)
-        {
-            _roomParticipantService = roomParticipantService;
-        }
+    /// <summary>
+    /// Getting a list of room participants
+    /// </summary>
+    /// <param name="request">Page Parameters</param>
+    /// <returns>List of room participants</returns>
+    [Authorize(policy: GulagSecurePolicy.User)]
+    [HttpGet]
+    [ProducesResponseType(typeof(RoomParticipantDetail), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(MessageResponse), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(MessageResponse), StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(typeof(MessageResponse), StatusCodes.Status500InternalServerError)]
+    public Task<ActionResult<RoomParticipantDetail>> GetParticipant([FromQuery] RoomParticipantGetRequest request)
+    {
+        return _roomParticipantService.GetParticipantAsync(request, HttpContext.RequestAborted).ToResponseAsync();
+    }
 
-        [Authorize(policy: GulagSecurePolicy.User)]
-        [HttpGet(nameof(GetParticipant))]
-        [ProducesResponseType(typeof(RoomParticipantDetail), StatusCodes.Status200OK)]
-        [ProducesResponseType(typeof(MessageResponse), StatusCodes.Status404NotFound)]
-        [ProducesResponseType(typeof(MessageResponse), StatusCodes.Status400BadRequest)]
-        public Task<ActionResult<RoomParticipantDetail>> GetParticipant(
-            [FromQuery] RoomParticipantGetRequest request)
-        {
-            return _roomParticipantService.GetParticipantAsync(request, HttpContext.RequestAborted).ToResponseAsync();
-        }
+    /// <summary>
+    /// Adding a transaction participant to a room
+    /// </summary>
+    /// <param name="request">Data for adding a participant to a room</param>
+    /// <returns>Data about the added participant to the room</returns>
+    [Authorize(policy: GulagSecurePolicy.Manager)]
+    [HttpPost]
+    [ProducesResponseType(typeof(RoomParticipantDetail), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(MessageResponse), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(MessageResponse), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(MessageResponse), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(MessageResponse), StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(typeof(MessageResponse), StatusCodes.Status500InternalServerError)]
+    public Task<ActionResult<RoomParticipantDetail>> CreateParticipant([FromBody] RoomParticipantCreateRequest request)
+    {
+        return _roomParticipantService.CreateParticipantAsync(request, HttpContext.RequestAborted).ToResponseAsync();
+    }
 
-        [Authorize(policy: GulagSecurePolicy.Manager)]
-        [HttpPost(nameof(CreateParticipant))]
-        [ProducesResponseType(typeof(RoomParticipantDetail), StatusCodes.Status200OK)]
-        [ProducesResponseType(typeof(MessageResponse), StatusCodes.Status404NotFound)]
-        [ProducesResponseType(typeof(MessageResponse), StatusCodes.Status400BadRequest)]
-        public Task<ActionResult<RoomParticipantDetail>> CreateParticipant(
-            [FromBody] RoomParticipantCreateRequest request)
-        {
-            return _roomParticipantService.CreateParticipantAsync(request, HttpContext.RequestAborted).ToResponseAsync();
-        }
-
-        [Authorize(policy: GulagSecurePolicy.Manager)]
-        [HttpPatch(nameof(ChangeParticipantStatus))]
-        [ProducesResponseType(typeof(RoomParticipantDetail), StatusCodes.Status200OK)]
-        [ProducesResponseType(typeof(MessageResponse), StatusCodes.Status404NotFound)]
-        [ProducesResponseType(typeof(MessageResponse), StatusCodes.Status400BadRequest)]
-        public Task<ActionResult<RoomParticipantDetail>> ChangeParticipantStatus(
-            [FromBody] RoomParticipantChangeStatusRequest request)
-        {
-            return _roomParticipantService.ChangeParticipantStatusAsync(request, HttpContext.RequestAborted).ToResponseAsync();
-        }
+    /// <summary>
+    /// Changing the status of a room participant
+    /// </summary>
+    /// <param name="request">Data changes in the status of a room participant</param>
+    /// <returns>Information about the participant of the room</returns>
+    [Authorize(policy: GulagSecurePolicy.Manager)]
+    [HttpPatch]
+    [ProducesResponseType(typeof(RoomParticipantDetail), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(MessageResponse), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(MessageResponse), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(MessageResponse), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(MessageResponse), StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(typeof(MessageResponse), StatusCodes.Status500InternalServerError)]
+    public Task<ActionResult<RoomParticipantDetail>> ChangeParticipantStatus(
+        [FromBody] RoomParticipantChangeStatusRequest request)
+    {
+        return _roomParticipantService.ChangeParticipantStatusAsync(request, HttpContext.RequestAborted)
+            .ToResponseAsync();
     }
 }
