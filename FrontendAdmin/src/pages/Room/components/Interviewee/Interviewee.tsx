1a98e3b40160e392d8fbbe664fc35b317de0fade
VladislavPetyukevich <pet.vladislav@gmail.com>
2023-10-23 20:57:59 +0300

    Frontend, Admin - Post websocket messages to iframe

--- FrontendAdmin/src/pages/Room/components/Interviewee/Interviewee.tsx
+++ FrontendAdmin/src/pages/Room/components/Interviewee/Interviewee.tsx
@@ -1,19 +1,23 @@
-import { FunctionComponent } from 'react';
+import { forwardRef } from 'react';
 import { REACT_APP_INTERVIEW_FRONTEND_URL } from '../../../../config';
 
 export interface IntervieweeProps {
   roomId: string;
 }
 
-export const Interviewee: FunctionComponent<IntervieweeProps> = ({
-  roomId,
-}) => {
+export const Interviewee = forwardRef<HTMLIFrameElement, IntervieweeProps>((
+  {
+    roomId,
+  },
+  ref
+) => {
   return (
     <iframe
       title="interviewee-client-frame"
       className="interviewee-frame"
+      ref={ref}
       src={`${REACT_APP_INTERVIEW_FRONTEND_URL}/?roomId=${roomId}`}
     >
     </iframe>
   )
-};
+});
