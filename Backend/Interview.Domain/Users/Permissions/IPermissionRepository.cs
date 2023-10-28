69b8601dee9bff774a1432a29963bee4f144a9d0
Dmitry Sushchevsky <d.suschevsky.personal@gmail.com>
2023-10-28 10:59:00 -0700

    Backend, UserService - Added new default permissions for user (#252)
    * Backend, UserService - Added new default permissions for user and added additional permissions that are not available to the user
    
    * Backend, RoleName - Added RoomAddParticipant to DefaultPermissions for 'user'
    
    * Fix QuestionServicePermissionAccessor
    
    * Added QuestionFindPageArchive to DefaultPermissions
    
    * Backend, RoleName - Added QuestionFindById as default for user

--- Backend/Interview.Domain/Users/Permissions/IPermissionRepository.cs
+++ Backend/Interview.Domain/Users/Permissions/IPermissionRepository.cs
@@ -1,9 +1,7 @@
-using Interview.Domain.Permissions;
 using Interview.Domain.Repository;
 
 namespace Interview.Domain.Users.Permissions;
 
 public interface IPermissionRepository : IRepository<Permission>
 {
-    public Task<List<Permission>> FindAllByTypes(HashSet<SEPermission> types, CancellationToken cancellationToken = default);
 }
