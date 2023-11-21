7b7026c2209bf5b92aed3c9f7fc3e7dbe2e9fd3a
Dmitry Sushchevsky <d.suschevsky.personal@gmail.com>
2023-11-21 22:19:53 +0400

    Backend, EventStorage2DatabaseServiceTest: Improved

--- Backend/Interview.Test/Integrations/EventStorage2DatabaseServiceTest.cs
+++ Backend/Interview.Test/Integrations/EventStorage2DatabaseServiceTest.cs
@@ -76,12 +76,16 @@ public class EventStorage2DatabaseServiceTest
         await service.ProcessAsync(CancellationToken.None);
 
         var actualEvents = await appDbContext.RoomEvents.ToListAsync();
+        var actualStorageEvents = await eventStorage
+            .GetBySpecAsync(Spec<IStorageEvent>.Any, 100, CancellationToken.None)
+            .ToListAsync();
 
         initialEvents.Should().BeEmpty();
         actualEvents.Should().HaveSameCount(storageEvents)
             .And.ContainSingle(e => IsSame(e, storageEvents[0]))
             .And.ContainSingle(e => IsSame(e, storageEvents[1]))
             .And.ContainSingle(e => IsSame(e, storageEvents[2]));
+        actualStorageEvents.SelectMany(e => e).Count().Should().Be(0);
     }
 
     private bool IsSame(DbRoomEvent roomEvent, StorageEvent storageEvent)
@@ -91,34 +95,5 @@ public class EventStorage2DatabaseServiceTest
                roomEvent.Payload == storageEvent.Payload &&
                roomEvent.Type == storageEvent.Type &&
                roomEvent.Id == storageEvent.Id;
-    }
-}
-
-public sealed class InMemoryEventStorage : IEventStorage
-{
-    private readonly List<IStorageEvent> _storage = new();
-
-    public ValueTask AddAsync(IStorageEvent @event, CancellationToken cancellationToken)
-    {
-        _storage.Add(@event);
-        return ValueTask.CompletedTask;
-    }
-
-    public async IAsyncEnumerable<IReadOnlyCollection<IStorageEvent>> GetBySpecAsync(ISpecification<IStorageEvent> spec, int chunkSize, CancellationToken cancellationToken)
-    {
-        await foreach (var res in _storage.Where(spec.IsSatisfiedBy).Chunk(chunkSize).ToAsyncEnumerable().WithCancellation(cancellationToken))
-        {
-            yield return res;
-        }
-    }
-
-    public ValueTask DeleteAsync(IEnumerable<IStorageEvent> items, CancellationToken cancellationToken)
-    {
-        foreach (var e in items)
-        {
-            _storage.Remove(e);
-        }
-            
-        return ValueTask.CompletedTask;
     }
 }
