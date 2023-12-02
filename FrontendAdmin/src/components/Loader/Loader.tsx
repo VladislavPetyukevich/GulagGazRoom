aa8f37098da6074b56c7d9df8704e6de974a41f1
VladislavPetyukevich <pet.vladislav@gmail.com>
2023-12-02 15:28:47 +0300

    Frontend - Fixed height for loader

--- FrontendAdmin/src/components/Loader/Loader.tsx
+++ FrontendAdmin/src/components/Loader/Loader.tsx
@@ -2,4 +2,8 @@ import React, { FunctionComponent } from 'react';
 
 import './Loader.css';
 
-export const Loader: FunctionComponent = () => <div className="loader">Loading...</div>;
+export const Loader: FunctionComponent = () => (
+  <div className="loader-wrapper">
+    <div className="loader">Loading...</div>
+  </div>
+);
